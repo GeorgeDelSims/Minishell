c94fa6237379df31dc1952cbee72e877a5a296a5
George Sims <gsims@c1r11s6.42lausanne.ch>
2024-03-11 17:56:43 +0100

    nouvelle struct pour les tokens - je crois que je comprends mieux le parsing maintenant

--- srcs/listutils.c
+++ srcs/listutils.c
@@ -6,23 +6,54 @@
 /*   By: gsims <gsims@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:57:29 by georgesims        #+#    #+#             */
-/*   Updated: 2024/03/11 15:40:26 by gsims            ###   ########.fr       */
+/*   Updated: 2024/03/11 17:55:17 by gsims            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 // Fonction pour creer nouveau node ou nouvelle liste chainee de commandes
-// il faudra surement les arguments de la commande en args de la fonction
-t_cmd	*ft_cmd_new(char *cmd, char **args)
+t_token	*ft_token_new(char *content)
 {
-	t_cmd	*new;
+	t_token	*new;
 	
-	new = (t_cmd *)malloc(sizeof(t_cmd));
+	new = (t_token *)malloc(sizeof(t_token));
 	if (!new)
 		return (NULL);
-	new->cmd = cmd;
-	new->args = args;
+	new->content = content;
+	new->in = 0;
+	new->out = 1;
+	new->cmd_path = NULL;
+	new->args = NULL;
+	new->type = 0;
 	new->next = NULL;
 	return (new);
 }
+
+// Ajouter node a la fin de la liste de tokens 
+void	ft_add_back(t_token **token, t_token *new)
+{
+	t_token	*curr;
+	
+	if (!token || !new)
+		return ;
+	if (*token = NULL)
+	{
+		*token = new;
+		return ;
+	}
+	curr = *token;
+	while (curr->next)
+		curr = curr->next;
+	curr->next = new;
+}
+
+// Ajouter node au debut de la liste de tokens
+void	ft_add_front(t_token **token, t_token *new)
+{
+	if (!token || !new)
+		return ;
+	new->next = *token;
+	*token = new;
+}
+
